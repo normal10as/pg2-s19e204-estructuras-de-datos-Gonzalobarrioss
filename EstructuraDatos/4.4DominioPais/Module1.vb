5850cf73efd2a9bad440f8a44569bf2062613468
gonza <gonza.barrios.geb@gmail.com>
2019-07-03 18:01:34 -0300

    4.4 fin

---
+++ EstructuraDatos/4.4DominioPais/Module1.vb
@@ -0,0 +1,28 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim dominioPais As New Collection
+        Dim dominio As String
+        dominioPais.Add("Argentina", "arg")
+        dominioPais.Add("España", "esp")
+        dominioPais.Add("Uruguay", "uru")
+        dominioPais.Add("Estados Unidos", "usa")
+        Do
+            Console.WriteLine("Ingresar dominio de pais: ")
+            dominio = Console.ReadLine()
+            If dominio = "" Then
+                Exit Do
+            End If
+            For Each dom In dominioPais
+                If dominioPais.Contains(dominio) Then
+                    Console.WriteLine("El nombre del pais es: " & dominioPais(dominio))
+                    Exit For
+                Else
+                    Console.WriteLine("No existe ese dominio.")
+                    Exit For
+                End If
+            Next
+        Loop
+    End Sub
+
+End Module
